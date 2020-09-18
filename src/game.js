1b4da469ec1292e0a9d2ee97ade4999f17551a17
JP Carey <jpcarey4@gmail.com>
2020-09-18 12:57:19 -0600

    Add cards array to game class

--- src/game.js
+++ src/game.js
@@ -1,9 +1,63 @@
 class Game {
-  constructor(gameCount, kitty) {
-    this.gameCount = gameCount;
+  constructor(kitty) {
     this.kitty = kitty;
+    this.gameCount = 0;
     this.player1 = new Player(hand, turn, wins);
     this.player2 = new Player(hand, turn, wins);
+    this.cardDeck = [
+      { src: "./assets/blue-01.png", number: 1, color: "blue" },
+      { src: "./assets/blue-02.png", number: 2, color: "blue" },
+      { src: "./assets/blue-03.png", number: 3, color: "blue" },
+      { src: "./assets/blue-04.png", number: 4, color: "blue" },
+      { src: "./assets/blue-05.png", number: 5, color: "blue" },
+      { src: "./assets/blue-06.png", number: 6, color: "blue" },
+      { src: "./assets/blue-07.png", number: 7, color: "blue" },
+      { src: "./assets/blue-08.png", number: 8, color: "blue" },
+      { src: "./assets/blue-09.png", number: 9, color: "blue" },
+      { src: "./assets/blue-10.png", number: 10, color: "blue" },
+      { src: "./assets/blue-jack.png", number: 11, color: "blue" },
+      { src: "./assets/blue-king.png", number: 13, color: "blue" },
+      { src: "./assets/blue-queen.png", number: 12, color: "blue" },
+      { src: "./assets/gold-01.png", number: 1, color: "gold" },
+      { src: "./assets/gold-02.png", number: 2, color: "gold" },
+      { src: "./assets/gold-03.png", number: 3, color: "gold" },
+      { src: "./assets/gold-04.png", number: 4, color: "gold" },
+      { src: "./assets/gold-05.png", number: 5, color: "gold" },
+      { src: "./assets/gold-06.png", number: 6, color: "gold" },
+      { src: "./assets/gold-07.png", number: 7, color: "gold" },
+      { src: "./assets/gold-08.png", number: 8, color: "gold" },
+      { src: "./assets/gold-09.png", number: 9, color: "gold" },
+      { src: "./assets/gold-10.png", number: 10, color: "gold" },
+      { src: "./assets/gold-jack.png", number: 11, color: "gold" },
+      { src: "./assets/gold-king.png", number: 13, color: "gold" },
+      { src: "./assets/gold-queen.png", number: 12, color: "gold" },
+      { src: "./assets/green-01.png", number: 1, color: "green" },
+      { src: "./assets/green-02.png", number: 2, color: "green" },
+      { src: "./assets/green-03.png", number: 3, color: "green" },
+      { src: "./assets/green-04.png", number: 4, color: "green" },
+      { src: "./assets/green-05.png", number: 5, color: "green" },
+      { src: "./assets/green-06.png", number: 6, color: "green" },
+      { src: "./assets/green-07.png", number: 7, color: "green" },
+      { src: "./assets/green-08.png", number: 8, color: "green" },
+      { src: "./assets/green-09.png", number: 9, color: "green" },
+      { src: "./assets/green-10.png", number: 10, color: "green" },
+      { src: "./assets/green-jack.png", number: 11, color: "green" },
+      { src: "./assets/green-king.png", number: 13, color: "green" },
+      { src: "./assets/green-queen.png", number: 12, color: "green" },
+      { src: "./assets/red-01.png", number: 1, color: "red" },
+      { src: "./assets/red-02.png", number: 2, color: "red" },
+      { src: "./assets/red-03.png", number: 3, color: "red" },
+      { src: "./assets/red-04.png", number: 4, color: "red" },
+      { src: "./assets/red-05.png", number: 5, color: "red" },
+      { src: "./assets/red-06.png", number: 6, color: "red" },
+      { src: "./assets/red-07.png", number: 7, color: "red" },
+      { src: "./assets/red-08.png", number: 8, color: "red" },
+      { src: "./assets/red-09.png", number: 9, color: "red" },
+      { src: "./assets/red-10.png", number: 10, color: "red" },
+      { src: "./assets/red-jack.png", number: 11, color: "red" },
+      { src: "./assets/red-king.png", number: 13, color: "red" },
+      { src: "./assets/red-queen.png", number: 12, color: "red" },
+    ];
   }
 
   shuffleDeck() {
@@ -15,7 +69,7 @@ class Game {
   }
 
   playHand() {
-    
+
   }
 
   slap() {
