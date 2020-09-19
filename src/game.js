f05b85d496aadb41f5a2b39966f0a9ac5290f4d2
JP Carey <jpcarey4@gmail.com>
2020-09-19 17:18:02 -0600

    Refactor game class to run survival round in existing method

--- src/game.js
+++ src/game.js
@@ -91,35 +91,33 @@ class Game {
     if (event.key === "q" && this.player1.turn) {
       this.kitty.unshift(this.player1.hand[0]);
       this.player1.hand.shift();
-      this.player1.turn = false;
-      this.player2.turn = true;
+      this.toggleTurns(event);
     } else if (event.key === "p" && this.player2.turn) {
       this.kitty.unshift(this.player2.hand[0]);
       this.player2.hand.shift();
-      this.player2.turn = false;
+      this.toggleTurns(event);
+    }
+  }
+
+  toggleTurns() {
+    if (this.player1.turn && this.player1.hand.length > 0 && this.player2.hand.length > 0) {
+      this.player1.turn = false;
+      this.player2.turn = true;
+    } else if (this.player2.turn && this.player1.hand.length > 0 && this.player2.hand.length > 0) {
       this.player1.turn = true;
+      this.player2.turn = false;
     }
   }
 
   overrideTurn(event) {
-    if (this.player1.hand.length === 0) {
+    if (this.player1.hand.length === 0 && this.player1.turn) {
       this.player1.turn = false;
       this.player2.turn = true;
-    } else if (this.player2.hand.length === 0) {
+    } else if (this.player2.hand.length === 0 && this.player2.turn) {
       this.player2.turn = false;
       this.player1.turn = true;
     }
-    this.playSurvivalRound(event);
-  }
-
-  playSurvivalRound(event) {
-    if (event.key === "q" && this.player1.turn) {
-      this.kitty.unshift(this.player1.hand[0]);
-      this.player1.hand.shift();
-    } else if (event.key === "p" && this.player2.turn) {
-      this.kitty.unshift(this.player2.hand[0]);
-      this.player2.hand.shift();
-    }
+    this.playHand(event);
   }
 
   slapCorrectly(event) {
